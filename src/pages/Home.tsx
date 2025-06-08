c16d3e250f9a9031548256e202e3e27f37cb3e31
Jacob Roelofsz <87379740+jroelofsz@users.noreply.github.com>
2025-06-08 01:43:05 -0500

    updated entire home page, removed placeholder text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,20 +5,29 @@ export default function Home() {
         <div className="container text-white text-center">
             <h1 className='page-header'>Home</h1>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Welcome to Lebubble Scripts! We're excited to share our collection of FiveM scripts with you. Our scripts are designed to enhance your gaming experience, providing unique features and functionalities for your server. 
+                <br></br>
+                <br></br>
+                Each script is designed with the end-user in mind, ensuring ease of use and integration. 
+                <br></br>
+                <br></br>
+                If you have any problems with a script, please open an ticket in the <a href="https://discord.gg/CUX8hVnswZ" target="_blank" rel="noopener noreferrer" className="text-info">Discord</a> server.
             </p>
             <hr></hr>
             <br></br>
-            <h3>Upcoming Scripts</h3>
-            <div className="d-flex justify-content-center">
-                <div className="card bg-dark text-white shadow-lg" style={{ maxWidth: '28rem' }}>
+            <h3>Most Recent Script</h3>
+            <div className="d-flex justify-content-center mb-4">
+                <div className="card bg-dark text-white" style={{ maxWidth: '28rem' }}>
                     <div className="card-body">
-                        <h3 className="card-title mb-3">lbs_drugun</h3>
-                        <h6 className="card-subtitle mb-2 text-info">In Development</h6>
-                        <p className="card-text">
-                            A QBCore drug running script designed to enhance the gameplay experience with immersive features and robust functionality. This script is currently under development, focusing on creating a seamless and engaging drug running experience for players.
-                        </p>
-                        <span className="badge bg-primary">Work in Progress</span>
+                        <a target="_blank" rel="noopener noreferrer" href="https://lebubble-scripts.tebex.io/package/6865798" className="text-decoration-none text-white">
+                            <h3 className="card-title mb-3">lbs_drugun</h3>
+                            <img src="../images/lbs_drugrun_400.png" className="rounded mb-4" alt="Item 3" />
+                            <h6 className="card-subtitle mb-2 text-info">Released</h6>
+                            <p className="card-text">
+                                A QBCore drug running script designed to enhance the gameplay experience with immersive features and robust functionality. This script is currently under development, focusing on creating a seamless and engaging drug running experience for players.
+                            </p>
+                            <span className="badge bg-primary">1.0</span>
+                        </a>
                     </div>
                 </div>
             </div>
